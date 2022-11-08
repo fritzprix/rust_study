6c99f1332de0027a8dff9c8fe7635716042eee23
fritzprix <innocentevil0914@gmail.com>
2022-11-09 06:43:26 +0900

    list

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,37 +1,83 @@
 
-use std::io::Write;
+use std::fmt::Debug;
 
-fn gcd(mut n: u64, mut m: u64) -> u64 {
-    assert!(n != 0 && m != 0);
-    while m !=0 {
-        if m < n {
-            let t = m;
-            m = n;
-            n = t;
-        }
-        m = m % n;
+struct ListNode<T> {
+    next: Option<Box<ListNode<T>>>,
+    value: T,
+}
+
+
+struct List<T> {
+    entry: Option<Box<ListNode<T>>>
+}
+
+impl <T> List<T> where T: ToString + Sized {
+    fn new() -> Self {
+        List { entry: None }
     }
-    n
+}
+
+impl <T> ListNode<T> where T: ToString + Sized {
+    fn new(value: T) -> Self {
+        ListNode { next: None, value }
+    }
+
+    fn value(&self) -> &T {
+        &self.value
+    }
+}
+
+impl <T> Debug for List<T> where T: ToString + Sized {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut res = "[".to_string();
+        let mut cur = &self.entry;
+        while let Some(n) = cur {
+            res.push_str(n.value().to_string().as_str());
+            cur = &n.next;
+            if cur.is_some() {
+                res.push_str(", ")
+            }
+        }
+        res.push_str("]");
+        f.write_str(res.as_str())
+    }
+}
+
+impl <T> List<T> where T: ToString + Sized {
+    fn enqueue(&mut self, value: T) {
+        let mut cur = &mut self.entry;
+        loop {
+            match cur {
+                Some(c) => {
+                    cur = &mut c.next
+                },
+                _ => {
+                    let _ = std::mem::replace(cur, Some(Box::new(ListNode::new(value))));
+                    return;
+                }
+            };
+        }
+    }
+
+    fn dequeue(&mut self) -> Option<T> {
+        let head = std::mem::replace(&mut self.entry, None);
+        if let Some(n) = head {
+            let _ = std::mem::replace(&mut self.entry, n.next);
+            Some(n.value)
+        } else {
+            None
+        }
+    }
 }
 
 fn main() {
-    let mut numbers: Vec<u64> = vec![];
-    for arg in std::env::args().skip(1) {
-        numbers.push(u64::from_str_radix(&arg, 10).expect("error parsing arguments"))
+    let mut my_list = List::<u32>::new();
+    for i in 0..5 {
+        my_list.enqueue(i)
     }
-    if numbers.len() == 0 {
-        writeln!(std::io::stderr(), "Usage: gcd Number ...").unwrap();
-        std::process::exit(1);
-    }
+    println!("{:?}", my_list);
+    assert_eq!(my_list.dequeue(), Some(0u32));
+    println!("{:?}", my_list);
 
-    let mut d = numbers[0];
-    for m in &numbers[1..] {
-        d = gcd(d, *m);
-    }
-    println!("GCD of {:?} is {}", numbers, d);
 }
 
-#[test]
-fn test()  {
-    assert!(gcd(1, 2) == 1)
-}
